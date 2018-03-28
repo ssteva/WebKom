32894672a299ea29b0a92f6e8b20bd1030aaf0da
Stevo Sudjic <stevo.sudjic@gmail.com>
2018-03-28 21:12:01 +0200

    2803

---
+++ Mapping/NacinDostaveMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using webkom.Mapping;
+using webkom.Models;
+
+namespace webkom.Mapping
+{
+  public class NacinDostaveMap : EntitetMap<NacinDostave>
+  {
+    public NacinDostaveMap()
+    {
+         base.MapSubClass();
+    }
+    protected override void MapSubClass()
+    {
+      Table("tNacinDostave");
+      Id(x => x.Id).UnsavedValue("").GeneratedBy.Assigned();
+      Map(x=>x.Naziv);
+    }
+  }
+}
---
+++ Models/NacinDostave.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webkom.Models
+{
+    public class NacinDostave : Entitet
+    {
+        public virtual string Id { get; set; }
+        public virtual string Naziv  { get; set; }
+        
+    }
+}
+
+
